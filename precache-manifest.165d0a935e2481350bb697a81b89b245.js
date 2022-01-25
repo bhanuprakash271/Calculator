7944794ea167bdb1be83c5766aba1d686e0eaf9e
bhanu prakash <veerabhanuprakash264@gmail.com>
2022-01-25 18:46:30 -0500

    Updates

---
+++ precache-manifest.165d0a935e2481350bb697a81b89b245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6db8a94a381dd7271f2dac060df32c",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "5eb8ce9692a4a208fb37",
+    "url": "/Calculator/static/css/main.4ccf6d8d.chunk.css"
+  },
+  {
+    "revision": "1dee7ed5b1617d17bec4",
+    "url": "/Calculator/static/js/2.a8f8a3b2.chunk.js"
+  },
+  {
+    "revision": "74e1f4f7c1ca7f0bbbe1d90a3dfe851a",
+    "url": "/Calculator/static/js/2.a8f8a3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb8ce9692a4a208fb37",
+    "url": "/Calculator/static/js/main.413e93a6.chunk.js"
+  },
+  {
+    "revision": "314d08f05e14c21b0da1",
+    "url": "/Calculator/static/js/runtime-main.8b2e153c.js"
+  }
+]);
